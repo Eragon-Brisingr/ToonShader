26aed40096e6e612e280c251deca0c4abbbff836
duoxu <duoxu@tencent.com>
2021-05-12 17:52:48 +0800

    完善插件描述

--- Source/ToonShader/Private/ToonShader.cpp
+++ Source/ToonShader/Private/ToonShader.cpp
@@ -1,7 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "ToonShader.h"
-#include <Interfaces/IPluginManager.h>
 
 #define LOCTEXT_NAMESPACE "FToonShaderModule"
 
@@ -9,9 +8,6 @@ void FToonShaderModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
 
-	// 注意模块的加载时机为"LoadingPhase": "PostConfigInit"，否则打包会报Can't map virtual shader source path
-	const FString PluginShaderDir = FPaths::Combine(IPluginManager::Get().FindPlugin(TEXT("ToonShader"))->GetBaseDir(), TEXT("Shaders"));
-	AddShaderSourceDirectoryMapping(TEXT("/ToonShader"), PluginShaderDir);
 }
 
 void FToonShaderModule::ShutdownModule()
